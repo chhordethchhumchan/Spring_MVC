55aeb725025daa6810559d58aec445535db89c43
chhordeth <chhordethchhum@gmail.com>
2018-10-18 13:45:27 +0700

    spring mvc

---
+++ src/main/java/com/mcnc/web/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.mcnc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value={"/welcome"})
+	
+	public String welcome(){
+		
+		return "welcome";
+	}
+
+}
