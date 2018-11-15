175e56d1c34cd7a797150968a74a6080cbae2054
chhordeth <chhordethchhum@gmail.com>
2018-11-15 17:41:36 +0700

    Update user

--- src/main/webapp/WEB-INF/resources/static/script/useradd.js
+++ src/main/webapp/WEB-INF/resources/static/script/useradd.js
@@ -1,7 +1,74 @@
-$(function() {
+$(document).ready(function() {
+	    
+    var row =[];
+    $('#dataTable').DataTable( {
+       
+             ajax:{
+                   url : "/spring-mvc-mcnc/api/user/userlist",
+                   type : "get",
+                   dataType: 'json',
+                   contentType: "application/json",
+                   dataSrc:'',
+                   dataType: "json",
+                   error: function(data){
+                       alert();
+                   },
+                  
+             
+              },
+               columns: [
+                   
+                   {
+                       title:'<input type="checkbox"/>',
+                      "data": null,
+                      "searchable": false,
+                      "sortable": false,
+                      "orderable": false,
+                      "class": "dt-body-center",
+                      "render": function (data, type, row) {
+                          return "<input name='m-chk-row' data-toggle='modal' data-target='#userEdit' class='activate' value='" + row.id+ "' type='checkbox'/>";
+                      },
+                  },
+                  { 
+                      title:"ID",
+                      "data": "id",
+                      "render": function (data, type, row) {
+                          data = '<a class="edit-user" data-toggle="modal" data-target="#editUser" attr=' + row.id + ' ><u>' + data + '</u></a>';
+                           return data;;
+                      },
+                   },
+                  { 
+                       title:"Name",
+                       "data": "name"
+                       
+                  },
+                 
+                ]
+              
+               });
+          
+   });
+   $('#dataTable').on('click','a.edit-user', function (e) {
+         e.preventDefault();
+       var id = $(e.target).text();
+       console.log(id);			 
+              $.ajax({
+                    
+                     type: "get",
+                     url: "/spring-mvc-mcnc/api/user/getUserByID?id="+id+"",
+                     async: false,
+                     contentType: "application/json",
+                     dataType: "json",
+                     success: function(data){
+                       $('#editUser #id').val(data.id); 	
+                       $('#editUser #name').val(data.name); 			 
+                       
+                   }
+            });
+    }); 
+   
 	
-	
-	  $('button[type=submit]').click(function(e) {
+	  $('#save').click(function(e) {
 		   e.preventDefault();
 		   		var user = {
 		           id: $("#id").val(),
@@ -17,16 +84,18 @@ $(function() {
 			          contentType: "application/json",
 			          dataType: "json",
 			          success: function(data){
-			               alert("Data Save: ");
+			        	  alert("Data insert succes!")
+			        	  location.reload();
 			          },
 			         
 			 });
 		   
+			   
+		  
 	 }); 
-	    
-   
-});
- 
+	   
+    
+
 
    
 
